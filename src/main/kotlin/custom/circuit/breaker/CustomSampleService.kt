3be83796bc4b4c2fa78081f1713b714e3170df01
David Gomez <david.gomez@eydevelopment.com>
2019-06-21 19:30:11 +0200

    Add service for custom circuit breaker sample

---
+++ src/main/kotlin/custom/circuit/breaker/CustomSampleService.kt
@@ -0,0 +1,21 @@
+package packageName.spring.circuit.breaker
+
+import groovy.util.logging.Slf4j
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+import packageName.wrappers.LoggerWrapper
+
+@Service
+@Slf4j
+class CustomSampleService @Autowired constructor(private val logger: LoggerWrapper) {
+
+    fun run(): String {
+        logger.info("Calling external service...")
+        val random = Math.random()
+        if (random > 0.5) {
+            throw RuntimeException("Something was wrong...")
+        }
+        logger.info("Success calling external service")
+        return "Success calling external service"
+    }
+}
