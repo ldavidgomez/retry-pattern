da925d0368f10f325c94a42ce12eb0357512e383
David Gomez <david.gomez@eydevelopment.com>
2019-06-27 12:45:55 +0200

    Change Int to AtomicInt Add delay time for nex retry

--- src/main/kotlin/custom/retry/Retry.kt
+++ src/main/kotlin/custom/retry/Retry.kt
@@ -3,16 +3,31 @@ package packageName.custom.retry
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import packageName.wrappers.LoggerWrapper
+import java.time.Duration
+import java.time.LocalDateTime
+import java.time.ZoneOffset
+import java.util.concurrent.atomic.AtomicInteger
 
 @Service
 class Retry<T> @Autowired constructor(private val logger: LoggerWrapper) {
-    var retryCounter: Int = 0
+    var retryCounter: AtomicInteger = AtomicInteger(0)
     private val maxRetries: Int = 3
+    @Volatile
+    private var lastFailure: LocalDateTime? = null
+    private val delay = Duration.ofMillis(2000L)
+    private val countdownFlag: Boolean = false
+
+    private val isTimerExpired: Boolean
+        get() = lastFailure!!.plus(delay).isBefore(LocalDateTime.now())
+
+    private val nextTry: Long
+        get() = lastFailure!!.plus(delay).toInstant(ZoneOffset.UTC).toEpochMilli().minus(LocalDateTime.now().toInstant(ZoneOffset.UTC).toEpochMilli())
 
     fun run(action: () -> T): T {
         return try {
             action.invoke()
         } catch (e: Exception) {
+            lastFailure = LocalDateTime.now()
             retry(action)
         }
 
@@ -21,22 +36,29 @@ class Retry<T> @Autowired constructor(private val logger: LoggerWrapper) {
     @Throws(RuntimeException::class)
     private fun retry(action: () -> T): T {
         logger.error("FAILED - Command fails, will be retried $maxRetries times.")
-        retryCounter = 1
-        while (retryCounter < maxRetries) {
-            try {
-                return action.invoke()
-            } catch (ex: Exception) {
-                retryCounter++
-                logger.error("FAILED - Command fails on retry $retryCounter of $maxRetries error: $ex")
-                if (retryCounter >= maxRetries) {
-                    logger.info("Max retries exceeded.")
-                    break
-                }
-                Thread.sleep(5000L)
-            }
+        retryCounter.set(1)
+        while (retryCounter.get() < maxRetries) {
+            if (isTimerExpired) {
+                try {
+                    return action.invoke()
+                } catch (ex: Exception) {
+                    retryCounter.addAndGet(1)
+                    logger.error("FAILED - Command fails on retry $retryCounter of $maxRetries error: $ex")
+                    if (retryCounter.get() >= maxRetries) {
+                        logger.info("Max retries exceeded.")
+                        break
+                    }
 
+                }
+            } else {
+                countDown()
+            }
         }
         throw RuntimeException("Command fails on all of $maxRetries retries")
+    }
+
+    private fun countDown() {
+        if (countdownFlag) logger.info("Next try in $nextTry millis")
     }
 }
 
