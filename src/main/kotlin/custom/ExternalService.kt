9348a58a5d65b97e7ca7dc9285b6c56108d57421
David Gomez <david.gomez@eydevelopment.com>
2019-06-27 11:56:29 +0200

    Add retry service to external service

--- src/main/kotlin/custom/ExternalService.kt
+++ src/main/kotlin/custom/ExternalService.kt
@@ -3,18 +3,20 @@ package packageName.custom.circuit.breaker
 import groovy.util.logging.Slf4j
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import packageName.custom.retry.Retry
 import packageName.wrappers.LoggerWrapper
 
 @Service
 @Slf4j
-class ExternalService @Autowired constructor(private val logger: LoggerWrapper) {
+class ExternalService @Autowired constructor(private val logger: LoggerWrapper, private val retry: Retry<String>) {
 
     fun run(): String {
         logger.info("Calling external service...")
         val random = Math.random()
         if (random > 0.5) {
-            throw RuntimeException("Something was wrong...")
+            retry.run {throw RuntimeException("Something was wrong...")}
         }
+        retry.run{"Success calling external service"}
         logger.info("Success calling external service")
         return "Success calling external service"
     }
